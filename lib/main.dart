9e23ffd799f43a913d2217c17f3e3da77fc742d7
khaled Mohammad <khaledmohd202@gmail.com>
2025-09-10 20:25:42 +0300

    App Icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:qafeel/qafeel.dart';
 
-void main() {
-  runApp(const Qafeel());
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(const Qafeel());
+  });
 }
