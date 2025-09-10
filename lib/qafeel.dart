a7adcfc551898786893b4a4093d317a8fa06ce36
khaled Mohammad <khaledmohd202@gmail.com>
2025-09-10 18:53:20 +0300

    Fonts Setup "Avenir-Arabic & Poppins"

---
+++ lib/qafeel.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Qafeel extends StatelessWidget {
+  const Qafeel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Qafeel',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Text(''),
+    );
+  }
+}
